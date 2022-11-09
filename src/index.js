3937e322bd7e4cb84a64bd648f4d1ff2b71ff0b3
Andy Woodward <andy.woodward1967@gmail.com>
2022-11-09 16:17:47 +0000

    create an async action creator to avoid errors. refactor action creator

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import App from './components/App'
 import reducers from './reducers'
+import thunk from 'redux-thunk'
 
 const root = createRoot(document.getElementById('root'))
 
+const store = createStore(reducers, applyMiddleware(thunk))
+
 root.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
 )
